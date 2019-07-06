66de99f96f7cd49351d4ba50920ad2fe20253ab8
Trung Nguyen <trungnv1704@gmail.com>
2019-07-06 15:31:44 +0700

    init project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
 import './styles.css';
 
-const Header = () => <header>How Redux Saga Works</header>;
+const Header = () => <header>
+    <span className="logo">Trung Nguyen</span>
+    React Redux Saga
+    <div className="github">
+        <a
+            href="https://github.com/goodguy000/redux-saga-unplash"
+            target="_blank"
+            rel="noopener noreferrer"
+        >Github</a>
+    </div>
+</header>;
 
 export default Header;
--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -5,8 +5,6 @@ import './styles.css';
 import Button from '../Button';
 import Stats from '../Stats';
 
-const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
-
 class ImageGrid extends Component {
     state = {
         images: [],
